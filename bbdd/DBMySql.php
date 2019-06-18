65cae1cab658eb03c6581b24e2bd99a2abe11fa1
Byron <byronbl.12@gmail.com>
2019-06-18 16:19:26 +0100

    Conexion
    Nos conectamos a la db

---
+++ bbdd/DBMySql.php
@@ -0,0 +1,29 @@
+<?php
+
+class DBMySql extends BaseDeDatos {
+	protected $conexion;
+
+	public function __construct($servidor, $usuario, $password, $db, $puerto=3306, $tipo) {
+		parent::__construct($servidor, $usuario, $password, $db, $puerto, 'mysql');
+		$this->conexion = mysqli_connect("{$this->servidor}:{$this->puerto}", $this->usuario, $this->password, $this->db);
+		mysqli_select_db($this->conexion, $this->db);
+	}
+
+	public function setQuery($query) {
+		//$query = mysqli::real_escape_string($query);
+		return $this->idConsulta = mysqli_query($this->conexion, $query);
+	}
+
+	public function queryToArray() {
+		return mysqli_fetch_assoc($this->idConsulta);
+	}
+
+	public function __destruct() {
+		mysqli_close($this->conexion);
+	}
+
+	public function consulta($consulta) {
+		$this->conexion;
+		$this->setQuery($consulta);
+	}
+}
--- bbdd/conexion.php
+++ bbdd/conexion.php
@@ -1,6 +1,12 @@
 <?php
 include("BaseDeDatos.php");
+include("DBMySql.php");
 
-$prueba = new BaseDeDatos('localhost', 'root', '', 'bus', 3306, 'mysql');
+$dblocal = new DBMySql('localhost', 'autobuses', 'kBMXc5rXGjFQEODj', 'bus', 3306, 'mysql');
+$dblocal->setQuery('SELECT * FROM autobuses');
 
-$prueba->verConfiguracion();
+$dblocal->verConfiguracion();
+
+echo '<pre>';
+print_r($dblocal->queryToArray());
+echo '</pre>';
