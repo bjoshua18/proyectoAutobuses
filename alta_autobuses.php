284a0bcf04807c4ea1e35da8c3c34d7d9f768645
Byron <byronbl.12@gmail.com>
2019-06-19 17:11:17 +0100

    Terminado el formulario de alta_autobuses.php

---
+++ alta_autobuses.php
@@ -0,0 +1,41 @@
+<?php
+include("funciones.php");
+
+$menu = menu(1);
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<meta http-equiv="X-UA-Compatible" content="ie=edge">
+	<title>Alta autobuses</title>
+	<link rel="stylesheet" href="estilos.css">
+</head>
+<body>
+	<header>
+		<h1><img src="images/logo.png" alt="Logo"/></h1>
+		<h2>Alta Autobuses</h2>
+	</header>
+	<nav>
+		<ul>
+			<?= $menu ?>
+		</ul>
+	</nav>
+	<form action="">
+		<label for="nombre">Nombre</label>
+		<input type="text" name="nombre" id="nombre"/>
+
+		<label for="color">Color</label>
+		<input type="text" name="color" id="color"/>
+		
+		<label for="capacidad">Capacidad</label>
+		<input type="text" name="capacidad" id="capacidad"/>
+		<input type="submit" value="Dar de Alta"/>
+	</form>
+	<article id="autobus">
+		<img src="images/autobus.png" alt="autobus">
+	</article>
+</body>
+</html>
