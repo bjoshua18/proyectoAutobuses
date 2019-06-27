0ecd9053a13b80036a4ade9a9167e4c32ce89b3e
Byron <byronbl.12@gmail.com>
2019-06-27 03:41:44 +0100

    Ventana de formulario Terminada la ventana de formulario, pendiente de terminar editar y crear autobus

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 $(document).ready(() => {
 
 	// Esta funcion se ejecuta en cuanto inicia la app
-	fetchBuses();
+	fetchBuses()
 
 	// Listar los autobuses
 	function fetchBuses() {
@@ -13,14 +13,46 @@ $(document).ready(() => {
 				let result = ''
 				buses.forEach(bus => {
 					result += `
-					<h3>Nombre: <span>${bus.nombre}</span><a href='editar_autobuses.php?id=${bus.id}' class='editar'><img src='images/editar.png'/></a></h3>
-					<h4>Color: <span>${bus.color}</span></h4>
-					<h4>Capacidad: <span>${bus.capacidad}</span></h4>
+					<div class="autobus" busid="${bus.id}">
+						<h3>Nombre: <span>${bus.nombre}</span><a href='#' class='editarBus'><img src='images/editar.png'/></a></h3>
+						<h4>Color: <span>${bus.color}</span></h4>
+						<h4>Capacidad: <span>${bus.capacidad}</span></h4>
+					</div>
 					`
 				})
 				$('#autobuses').html(result)
 			}
 		})
+	}
+
+	// Editar autobus
+	$(document).on('click', '.editarBus', (e) => {
+		let element = e.target.parentElement.parentElement.parentElement // Obtenemos el elemento que tiene el id del bus
+		let id = $(element).attr('busid')
+		$.post('editar_autobuses.php', {id}, response => {
+			// Pendiente
+		})
+		e.preventDefault() // Evitar recarga
+	})
+
+	// Botón para abrir formulario para añadir un nuevo autobus
+	$('.nuevobus').on('click', e => {
+		showFormWindow()
+		e.preventDefault()
+	})
+
+	// Botón para cerrar ventana de formulario
+	$('#btn-cancelar').on('click', e => {
+		hideFormWindow()
+		e.preventDefault()
+	})
+
+	function showFormWindow() {
+		$('#form-container').show()
+	}
+
+	function hideFormWindow() {
+		$('#form-container').hide()
 	}
 
 })
